cc5168323fe589a0f232338fcb463387d45438cd
chetanp2402 <chetanp2402@gmail.com>
2022-08-10 10:24:27 +0530

    Collector grouping by

--- src/com/chetan/collectors/CollectorJoining.java
+++ src/com/chetan/collectors/CollectorJoining.java
@@ -1,6 +1,9 @@
 package com.chetan.collectors;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CollectorJoining {
@@ -15,9 +18,43 @@ public class CollectorJoining {
                 new Person("Harry", 19),
                 new Person("Sam", 20));
 
+        // Find person with min age
+        Optional<Person> minAgePersonOpt = persons.stream().min(Comparator.comparing(Person::getAge));
+        System.out.println(minAgePersonOpt);
+
         // joining names in single string with some delimiter
         String names = persons.stream().map(Person::getName).collect(Collectors.joining(", "));
 
         System.out.println(names);
+
+        //Grouping person objects by age using collector
+        // No downstream collector used
+        Map<Integer, List<Person>> mapByAge = persons.stream().collect(
+                Collectors.groupingBy(Person::getAge)
+        );
+
+        System.out.println(mapByAge);
+
+        //Grouping person objects by age using collector and counting
+        // Downstream collector used for counting
+        Map<Integer, Long> mapByAgeForCounting = persons.stream().collect(
+                Collectors.groupingBy(Person::getAge, Collectors.counting())
+        );
+
+        System.out.println(mapByAgeForCounting);
+
+        //Grouping person objects by age using collector and collecting names in a list
+        // Downstream collector used for counting
+        Map<Integer, List<String>> mapByAgeWithNameOfPerson = persons.stream().collect(
+                Collectors.groupingBy(
+                        Person::getAge,
+                        Collectors.mapping(Person::getName, Collectors.toList())
+                ));
+
+
+        System.out.println(mapByAgeWithNameOfPerson);
+
+
+
     }
 }
